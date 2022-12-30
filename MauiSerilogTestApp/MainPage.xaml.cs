68e9b33ca6bcfb75c8f8d2d6b0e465ded71f5bfa
skima74 <acaminschi@gmail.com>
2022-12-30 12:09:27 +0800

    More experiments, local network still not reachable

--- MauiSerilogTestApp/MainPage.xaml.cs
+++ MauiSerilogTestApp/MainPage.xaml.cs
@@ -11,9 +11,6 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-        LoggerFactory loggerFactory = new();
-        loggerFactory.AddSerilog(Log.Logger);
-        logger = loggerFactory.CreateLogger<App>();
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
@@ -21,15 +18,43 @@ public partial class MainPage : ContentPage
         count++;
         var countTxt = $"Clicked {count} time" + (count == 1 ? "" : "s");
         CounterBtn.Text = countTxt;
-        logger?.LogInformation(countTxt);
     }
 
     private async void OnCheckConnectivity(object sender, EventArgs e)
     {
         NetworkAccess status = Connectivity.Current.NetworkAccess;
-        //PermissionStatus status = await Permissions.CheckStatusAsync<Permissions>();
-        
         await Shell.Current.DisplayAlert("Connectivity Status", $"Current Status = {status}", "OK");
     }
+
+    private void OnInitLogger(object sender, EventArgs e)
+    {
+        string startupLoggerUrl = LoggerUrl.Text;
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Verbose()
+            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+            .MinimumLevel.Override("Apple", Serilog.Events.LogEventLevel.Warning)
+            .MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("DeviceID", "mauiApp")
+            .Enrich.WithClientAgent()
+            .Enrich.WithClientIp()
+            .Enrich.WithUserName()
+            .Enrich.WithAssemblyName()
+            .Enrich.WithAssemblyVersion()
+            .Enrich.WithThreadId()
+            .WriteTo.Seq(startupLoggerUrl)
+            .CreateLogger();
+
+        LoggerFactory loggerFactory = new();
+        loggerFactory.AddSerilog(Log.Logger);
+        logger = loggerFactory.CreateLogger<App>();
+    }
+
+    private void OnTestLogger(object sender, EventArgs e)
+    {
+        logger?.LogInformation("Logger Test Message : count = {count} ",count);
+    }
+
 }
 
--- MauiSerilogTestApp/MauiProgram.cs
+++ MauiSerilogTestApp/MauiProgram.cs
@@ -6,24 +6,6 @@ public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
-        string startupLoggerUrl = "http://10.0.0.105:4004";
-
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Verbose()
-            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
-            .MinimumLevel.Override("Apple", Serilog.Events.LogEventLevel.Warning)
-            .MinimumLevel.Override("System", Serilog.Events.LogEventLevel.Warning)
-            .Enrich.FromLogContext()
-            .Enrich.WithProperty("DeviceID", "mauiApp")
-            .Enrich.WithClientAgent()
-            .Enrich.WithClientIp()
-            .Enrich.WithUserName()
-            .Enrich.WithAssemblyName()
-            .Enrich.WithAssemblyVersion()
-            .Enrich.WithThreadId()
-            .WriteTo.Seq(startupLoggerUrl)
-            .CreateLogger();
-
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
@@ -32,7 +14,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             })
-            .Logging.AddSerilog(Log.Logger);
+            ;
 
 
         return builder.Build();
